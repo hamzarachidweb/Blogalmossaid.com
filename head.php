7625c3e354dc3e924db881f3809025d2339956ac
Hamza Rachid <80962663+hamzarachidweb@users.noreply.github.com>
2024-07-09 05:30:39 -0700

    Update head.php

--- head.php
+++ head.php
@@ -47,7 +47,6 @@ body {
   
     <!-- Bootstrap CSS -->
     <!-- Go to www.addthis.com/dashboard to customize your tools -->
-    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-61b9c0a196828fb6"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
     <link href="css/bootstrap.min.css" rel="stylesheet" >
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.2/css/all.css'>
